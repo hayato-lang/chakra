f2bfa1abcc848661bdaecbd7dde3cd36b620ea75
hayato-lang <j.h.14.s.g.8@gmail.com>
2021-12-14 18:31:15 +0900

    ルーティングの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import {
-  ChakraProvider,
-  Button
-} from "@chakra-ui/react";
-import theme from "./theme/theme"
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+
+import { Router } from "./router/Router";
+import theme from "./theme/theme";
 
 
 export const App = () => {
   return (
     <ChakraProvider theme={theme}>
-      <Button colorScheme="teal">ボタン</Button>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ChakraProvider>
   );
-}
+};
