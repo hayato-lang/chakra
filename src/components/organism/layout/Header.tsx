2e685f145e47b1588cf2d043aab3a2acb891657c
hayato-lang <j.h.14.s.g.8@gmail.com>
2021-12-15 10:19:08 +0900

    コンポーネント化

--- src/components/organism/layout/Header.tsx
+++ src/components/organism/layout/Header.tsx
@@ -1,18 +1,14 @@
-import { IconButton } from "@chakra-ui/button";
 import { 
   Flex, 
   Heading, 
-  Button,
   Box, 
   Link, 
-  Drawer, 
-  DrawerOverlay, 
-  DrawerContent,
-  DrawerBody,
   useDisclosure
  } from "@chakra-ui/react";
 import { memo, VFC } from "react";
-import { HamburgerIcon } from "@chakra-ui/icons";
+
+import { MenuIconButton } from "../../atoms/button/MenuIconButton"
+import { MenuDrawer } from "../../molcules/MenuDrawer";
 
 export const Header: VFC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -49,27 +45,10 @@ export const Header: VFC = memo(() => {
             <Link>ユーザー一覧</Link>
           </Box>
           <Link>設定</Link>
-          <IconButton 
-          aria-label="メニューボタン" 
-          icon={HamburgerIcon} 
-          size="sm" 
-          variant="unstyled" 
-          display={{ base:"block", md: "none" }}
-          onClick={onOpen}
-          />
         </Flex>
+        <MenuIconButton onOpen={onOpen}/>
       </Flex>
-      <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
-        <DrawerOverlay>
-          <DrawerContent>
-            <DrawerBody p={0} bg="gray.100">
-              <Button w="100%">TOP</Button>
-              <Button w="100%">ユーザー一覧</Button>
-              <Button w="100%">設定</Button>
-            </DrawerBody>
-          </DrawerContent>
-        </DrawerOverlay>
-      </Drawer>
+      <MenuDrawer onClose={onClose} isOpen={isOpen} />
     </>
   );
 });
