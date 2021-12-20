f84d6f345c764ff3315602ba76f61c1077bb93c7
hayato-lang <j.h.14.s.g.8@gmail.com>
2021-12-20 10:14:39 +0900

    モーダルのフォーマット作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,14 +1,33 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { Wrap, WrapItem, Center, Spinner } from "@chakra-ui/react";
-import { memo, useEffect, VFC } from "react";
+import { 
+  Wrap, 
+  WrapItem, 
+  Center, 
+  Spinner, 
+  Modal, 
+  ModalContent, 
+  ModalOverlay, 
+  ModalHeader, 
+  ModalCloseButton, 
+  ModalBody,
+  Stack,
+  FormControl,
+  FormLabel,
+  Input,
+  useDisclosure 
+} from "@chakra-ui/react";
+import { memo, useCallback, useEffect, VFC } from "react";
 import { useAllUsers } from "../../hooks/useAllUsers";
 
 import { UserCard } from "../organism/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const { getUsers, users, loading } = useAllUsers();
 
   useEffect(() => getUsers(), []);
+
+  const onClickUser = useCallback(() => onOpen(), []);
   return (
     <>
     {loading ? (
@@ -23,11 +42,40 @@ export const UserManagement: VFC = memo(() => {
                   imageUrl="https://source.unsplash.com/random"
                   userName={user.username}
                   fullName={user.name}
+                  onClick={onClickUser}
                   />
             </WrapItem>
         ))}
       </Wrap>
     )}
+    <Modal isOpen={isOpen} onClose={onClose} autoFocus={false}>
+      <ModalOverlay>
+        <ModalContent pb={6}>
+          <ModalHeader>ユーザー詳細</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody mx={4}>
+            <Stack spacing={4}>
+              <FormControl>
+                <FormLabel>名前</FormLabel>
+                <Input value="勇人" isReadOnly/>
+              </FormControl>
+              <FormControl>
+                <FormLabel>フルネーム</FormLabel>
+                <Input value="hayato tajima" isReadOnly/>
+              </FormControl>
+              <FormControl>
+                <FormLabel>Mail</FormLabel>
+                <Input value="aggerenrique@yahoo.co.jp" isReadOnly/>
+              </FormControl>
+              <FormControl>
+                <FormLabel>Tel</FormLabel>
+                <Input value="090-1234-5678" isReadOnly/>
+              </FormControl>
+            </Stack>
+          </ModalBody>
+        </ModalContent>
+      </ModalOverlay>
+    </Modal>
     </>
   );
 });
