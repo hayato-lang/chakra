41da7091767d6e158442bbb302533b1159a8877a
hayato-lang <j.h.14.s.g.8@gmail.com>
2021-12-20 14:43:10 +0900

    ユーザー詳細モーダル

---
+++ src/hooks/useSelectUser.ts
@@ -0,0 +1,16 @@
+import { useCallback, useState } from "react";
+import { User } from "../types/api/user";
+
+type Props = {
+  id: number;
+  users: Array<User>;
+}
+
+export const useSelectUser = () => {
+  const [ selectedUser, setSelectedUser ] = useState<User | null>(null);
+
+  const onSelectUser = useCallback((props: Props) => {
+    const { id, users } = props;
+  }, [])
+  return { onSelectUser, selectedUser }
+}
