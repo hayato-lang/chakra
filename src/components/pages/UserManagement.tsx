f1d226a343d72b583627b385d02915b48a46b1e7
hayato-lang <j.h.14.s.g.8@gmail.com>
2021-12-20 09:51:03 +0900

    ユーザー一覧取得機能の実装

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,18 +1,33 @@
-import { Wrap, WrapItem } from "@chakra-ui/react";
-import { memo, VFC } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Wrap, WrapItem, Center, Spinner } from "@chakra-ui/react";
+import { memo, useEffect, VFC } from "react";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 import { UserCard } from "../organism/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
+  const { getUsers, users, loading } = useAllUsers();
+
+  useEffect(() => getUsers(), []);
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-        <WrapItem>
-          <UserCard
-           imageUrl="https://source.unsplash.com/random"
-           userName="はやと"
-           fullName="Hayato Tajima"
-           />
-        </WrapItem>
-    </Wrap>
+    <>
+    {loading ? (
+      <Center h="100vh">
+        <Spinner />
+      </Center>
+    ): (
+      <Wrap p={{ base: 4, md: 10 }}>
+        {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+                  <UserCard
+                  imageUrl="https://source.unsplash.com/random"
+                  userName={user.username}
+                  fullName={user.name}
+                  />
+            </WrapItem>
+        ))}
+      </Wrap>
+    )}
+    </>
   );
 });
